7382636fdfb757deb33f427b4a1557e6b1bbd50d
Carlos Gomez <gomezkarlos1988@hotmail.com>
2020-05-25 17:44:09 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*aca ponemos el codigo de javascript*/
+
+function ocultar() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
